d6f588003924e2e87a8163d0535b5f54a1a98935
rose-web319 <Awotunderose@gmail.com>
2026-01-08 14:00:39 +0000

    auth procider

--- client/src/store/AuthProvider.jsx
+++ client/src/store/AuthProvider.jsx
@@ -1,8 +1,8 @@
 import { AuthProviderContext } from "@/hooks/useAuth";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { getAuthUser, logoutUser, refreshAccessToken } from "@/api/auth";
+import { useCallback, useState, useEffect } from "react";
 import LazySpinner from "@/components/LazySpinner";
-import { useCallback, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { jwtDecode } from "jwt-decode";
 import { safeGetItem, safeSetItem, safeRemoveItem } from "@/utils/storage";
@@ -11,13 +11,13 @@ export default function AuthProvider({ children }) {
   const [accessToken, setAccessToken] = useState(() => {
     return safeGetItem("laundryBookingToken") || null;
   });
-  const [isAuthenticating, setIsAuthenticating] = useState(false);
   const [user, setUser] = useState(null);
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
   const [bookingForm, setBookingForm] = useState(() => {
     const persistedState = safeGetItem("laundryBookingForm");
     return persistedState ? JSON.parse(persistedState) : null;
   });
-
   const queryClient = useQueryClient();
 
   useEffect(() => {
@@ -48,12 +48,6 @@ export default function AuthProvider({ children }) {
 
   useEffect(() => {
     let needsRefresh = false;
-
-    if (!accessToken) {
-      refreshTokenAction();
-      return;
-    }
-
     try {
       const decodedToken = jwtDecode(accessToken);
       const expirationTime = decodedToken?.exp ?? 0;
@@ -72,7 +66,7 @@ export default function AuthProvider({ children }) {
       return;
     }
 
-    //If token is valid and not expiring soon, fetch user
+    // If token is valid & not expiring soon, fetch user
     setIsAuthenticating(true);
     async function fetchUser() {
       try {
@@ -82,60 +76,27 @@ export default function AuthProvider({ children }) {
         }
       } catch (error) {
         console.error(error);
-        //if error, force refresh token
         await refreshTokenAction();
       } finally {
         setIsAuthenticating(false);
       }
     }
+
     fetchUser();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accessToken]);
 
-  //refresh accessToken
-  // const refresh = useQuery({
-  //   queryKey: ["refresh_token"],
-  //   queryFn: async () => {
-  //     const res = await refreshAccessToken();
-  //     if (res.status === 200) {
-  //       setAccessToken(res.data.data);
-  //     } else {
-  //       {
-  //         setAccessToken(null);
-  //       }
-  //     }
-  //     return res;
-  //   },
-  //   enabled: !accessToken,
-  //   retry: false,
-  // });
-
-  //fetch auth user
-  // const { isPending } = useQuery({
-  //   queryKey: ["auth_user", accessToken],
-  //   queryFn: async () => {
-  //     const res = await getAuthUser(accessToken);
-  //     if (res.status === 200) {
-  //       setUser(res.data.data);
-  //     } else {
-  //       setUser(null);
-  //     }
-  //     return res;
-  //   },
-  //enabled: !!accessToken, //run only when there is an accesstoken
-  // });
-
   const mutation = useMutation({
     mutationFn: logoutUser,
     onSuccess: (res) => {
-      toast.success(res?.data?.message || "Logout Successful");
+      toast.success(res?.data?.message || "Logout successful");
       queryClient.clear();
       setAccessToken(null);
       setUser(null);
     },
     onError: (err) => {
-      import.meta.env.DEV && console.error(err);
-      toast.error(err?.response?.data?.messsage || "Something went wrong");
+      console.error(err);
+      toast.error(err?.response?.data?.message || "Something went wrong");
     },
   });
 
